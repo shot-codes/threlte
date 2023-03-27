2cc4db79556ab58dac9b6cce9ede1e8cb2b0e4d4
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-03-27 02:05:39 +0200

    Added some stuff

--- apps/docs-next/src/examples/rapier/basic-car-controller/Scene.svelte
+++ apps/docs-next/src/examples/rapier/basic-car-controller/Scene.svelte
@@ -1,40 +1,29 @@
 <script lang="ts">
   import { T } from '@threlte/core'
-  import { Environment, OrbitControls } from '@threlte/extras'
   import { useRapier } from '@threlte/rapier'
   import { DEG2RAD } from 'three/src/math/MathUtils'
   import Car from './Car.svelte'
   import Level from './Level/Level.svelte'
-
-  const { world } = useRapier()
-  world.maxStabilizationIterations = 10
-  world.maxVelocityIterations = 10
-  world.maxVelocityFrictionIterations = 16
 </script>
-
-<Environment
-  path="/hdr/"
-  files="shanghai_riverside_1k.hdr"
-/>
 
 <T.DirectionalLight
   intensity={0.4}
   position={[8, 20, -3]}
 />
 
+<T.AmbientLight
+  intensity={0.2}
+  position={[8, 20, -3]}
+/>
+
 <Level />
 
-<Car
-  position.x={5}
-  position.y={5}
->
+<Car>
   <T.PerspectiveCamera
     rotation={[-90 * DEG2RAD, 70 * DEG2RAD, 90 * DEG2RAD]}
     position.x={10}
     position.y={5}
     fov={60}
     makeDefault
-  >
-    <!-- <OrbitControls /> -->
-  </T.PerspectiveCamera>
+  />
 </Car>
